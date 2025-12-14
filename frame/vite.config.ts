9f26edb2ae50af57bbdd837e9b2adfe169ea3c21
Harimohan Sharma <harry.g2016@gmail.com>
2025-12-14 13:20:47 +0530

    fix

--- frame/vite.config.ts
+++ frame/vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/frameIt/',   // 👈 REQUIRED for GitHub Pages
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
